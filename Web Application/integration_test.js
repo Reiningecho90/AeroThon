4409fba37f01fc9bb4c872a319e1387f067942d3
Reinier Johnson <trunkfish@gmail.com>
2023-03-28 17:41:44 -0400

    Editing and adding files
    Added node-modules to include PythonShell, this code throws errors, will be fixed soon

--- Web Application/integration_test.js
+++ Web Application/integration_test.js
@@ -3,3 +3,13 @@ This file includes the backend for the AeroThon website
 
 The below code communicates with the "physics_simualtion.py" file
 */
+
+const {PythonShell} = require('python-shell');
+
+var testData = [];
+
+PythonShell.run('Web Application\\base_data.py', function(err, results){
+    if (err) throw err;
+    console.log(results);
+    console.log('Done');
+});
