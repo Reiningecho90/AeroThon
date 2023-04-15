82b9b6379b61e828e560dbc44d3d24fe242a77b7
Reinier Johnson <trunkfish@gmail.com>
2023-04-15 17:47:29 -0400

    Fixed NodeJS>Python Implementation
    Fixed code to allow NodeJS to properly communicate with the base_data.py file via PythonShell.

--- Web Application/integration_test.js
+++ Web Application/integration_test.js
@@ -6,10 +6,14 @@ The below code communicates with the "physics_simualtion.py" file
 
 const {PythonShell} = require('python-shell');
 
-var testData = [];
 
-PythonShell.run('Web Application\\base_data.py', function(err, results){
-    if (err) throw err;
-    console.log(results);
-    console.log('Done');
+
+let options = {
+    args:["1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "True", "RTA3"]
+};
+
+PythonShell.run("../Python Backend\\base_data.py", options).then(results=>{
+    for (var i = 0; i<results.length; i++){
+        console.log(results[i]);
+    }
 });
