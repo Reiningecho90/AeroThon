8357aa63cd26904a3fb3b298090f5fd61fb1aee2
Reinier Johnson <trunkfish@gmail.com>
2023-05-01 21:22:35 -0400

    Updated NodeJS Integration
    Added more code to support future update where other python-computer algorithms will be added to base_data.py

--- Web Application/integration_test.js
+++ Web Application/integration_test.js
@@ -6,14 +6,34 @@ The below code communicates with the "physics_simualtion.py" file
 
 const {PythonShell} = require('python-shell');
 
-
+var condition = "False";
 
 let options = {
-    args:["1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "True", "RTA3"]
+    args:["1.0", "2.0", "3.0", "4.0", "5.0", "6.0", condition, "RTA3"]
 };
 
 PythonShell.run("../Python Backend\\base_data.py", options).then(results=>{
+    
+    var refList = options.args.slice(0, -2);
+    var newList = [];
+
     for (var i = 0; i<results.length; i++){
-        console.log(results[i]);
+        if ("False" == condition){
+            if (i <= 1){
+                continue;
+            }else{
+                console.log(results[i].slice(20, 25));
+                newList.push(results[i].slice(20, 25));
+            }
+        }else{
+            console.log(results[i]);
+        }
+    }
+    if (newList.toString() == refList.toString()){
+        console.log("LIST CHECK \u001b[32mOK\u001b[37m");
+    }else{
+        console.log("LIST CHECK \u001b[31mFAILED\u001b[37m");
+        console.log(newList);
+        console.log(refList);
     }
 });
